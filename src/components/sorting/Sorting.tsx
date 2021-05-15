129868fbd6d62869f15b1001ddcbc6516f1f0df3
Serkan KONAKCI <serkan.konakci@logo.com.tr>
2021-05-16 01:34:44 +0300

    refactor(sorting):update sorting ui

--- src/components/sorting/Sorting.tsx
+++ src/components/sorting/Sorting.tsx
@@ -1,16 +1,29 @@
-interface SortingProps {
-}
+import React from 'react';
 
-export const Sorting = (props: SortingProps): JSX.Element => {
+import './sorting.scss';
+
+export const Sorting = (): JSX.Element => {
   const items = [
     {label: 'Price low to high', type: 'asc'},
     {label: 'Price high to low', type: 'desc'},
     {label: 'New to old', type: 'date'},
     {label: 'Old to new', type: 'date'},
   ];
+
+  const idGenerator = () => {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
+
+  const id = idGenerator();
+
   return (
-    <>
-      {items.map(((item,key) => <li key={key}>{item.label}</li>))}
-    </>
+    <ul className="sorting-list">{
+      items.map((item, key) =>
+        <li key={key}>
+          <input id={`${key}-${id}`} type="radio" name={id} onChange={() => console.log(item)}/>
+          <label htmlFor={`${key}-${id}`}>{item.label}</label>
+        </li>,
+      )}
+    </ul>
   )
 }
