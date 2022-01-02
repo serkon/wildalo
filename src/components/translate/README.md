b3654eb85509f527dec42d07b3f66c01a44cb31a
Serkan KONAKCI <serkankonakci@gmail.com>
2022-01-02 23:47:20 +0300

    feat: create translation component

---
+++ src/components/translate/README.md
@@ -0,0 +1,52 @@
+# Translate
+
+Bu script uygulama içerisindeki dil değişimini yapmayı sağlar. Hızlı ve basit bir şekilde React tabanlı uygulamalara eklenebilmesi için yazdım. Entegrasyon işlemi 2 adımdan oluşuyor:
+
+### 1. Adım
+React'ın en tepesine `Language` class'ına ait tag'i eklemeniz gerekir. Daha doğrusu ilk nereden başlamasını istiyorsanız bu `Language` sınıfını 
+
+```typescript
+<Language>
+  <App />
+</Language>
+```
+
+### 2. Adım
+Kullanmak istediğiniz yere context ekleyerek dil değişim işlemlerini yapabilirsiniz. 3 adet değer dönüyor.
+
+- t: Translate fonksiyonu: 2 adet parametre alıyor: key ve param. Dil dosyası içerisindeki key'e bakar ve parametre varsa günceller. Örneğin: `t{'hello', {count: 2}}`
+- tChange: Dil değiştirmeye yarar. Örneğin: `tChange('tr')`
+- tState: Dil ile ilgili değerleri döner. Örneğin: `{language: 'tr', content: {hello: 'Merhaba, count {param}'}}`
+
+2 adet örnek ekledim. Function ve Class componentler için örnekleri bulabilirsiniz.
+
+```typescript
+// sample 1
+export const AnimalCardWithFunction = (props: React.PropsWithChildren<Props>): JSX.Element => {
+  const { t, tChange, tState } = useTranslate();
+  const classes = ['content'];
+  if (props.className) {
+    classes.push(props.className);
+  }
+  return (
+    <Box className="animal-card">
+      <Button onClick={()=>tChange('tr')}>{t('withParam', { 'param': 'level 2' })}</Button>
+    </Box>
+  );
+};
+```
+
+```typescript
+// sample 2
+export class AnimalCardWithClass extends React.Component<Props> {
+  static contextType = LanguageContext;
+
+  render(): React.ReactNode {
+    return (
+      <Box className="animal-card">
+        <Button>{this.context.t('withParam', { 'param': 'level 2' })}</Button>
+      </Box>
+    );
+  }
+}
+```
---
+++ src/components/translate/translate.component.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export const LanguageContext = React.createContext<{
+  tState: State;
+  tChange: (language: string) => Promise<void>;
+  t: (key: string, params?: any) => string;
+}>({
+  'tState': { 'language': 'en', 'content': {} },
+  'tChange': () => new Promise((resolve) => resolve()),
+  't': () => '',
+});
+
+interface Props {}
+
+interface State {
+  language: string;
+  content: { [key: string]: string };
+}
+
+export class Language extends React.Component<Props, State> {
+  content: { [key: string]: string } = {};
+  static contextType = LanguageContext; // alternatif: LanguageContext.contextType = SampleContext;
+  state = { 'language': 'en', 'content': {} };
+
+  componentDidMount() {
+    this.changeLanguage();
+  }
+
+  changeLanguage = async (language = 'en') => {
+    const content = await import(`./languages/${language}.json`);
+    this.setState({ language, content }, () => {
+      console.log(this.state);
+    });
+  };
+
+  translate = (key: string, params?: any) => {
+    let text = Language.getObjectPathValue(this.state.content, key) || key;
+    const type = params !== null && typeof params !== 'undefined' ? params.constructor.name : null;
+    if (type === 'Array') {
+      params.forEach((param: any, paramIndex: number) => {
+        text = text.replace(new RegExp(`{${paramIndex}}`, 'g'), param);
+      });
+    } else if (type === 'Object') {
+      Object.keys(params).forEach((keyName: string) => {
+        text = text.replace(new RegExp(`{${keyName}}`, 'g'), params[keyName]);
+      });
+    }
+    return text;
+  };
+
+  /**
+   * Get value by given path of object
+   * @example
+   * var m = {a: {b:1, c: {d: {e: [1,2,3]}}}}
+   * Util.getObjectPathValue(m, "a.c.d.e") // result is (3) [1, 2, 3]
+   * @param value - The object from which to import data
+   * @param path - String path of the target property
+   */
+  static getObjectPathValue(value: any, path: string) {
+    console.log('getObject: ', value, path);
+    let data = value;
+    if (path) {
+      path = path.toString();
+      path.split('.').forEach((val: any) => {
+        data = data !== null && typeof data !== 'undefined' ? data[val] : null;
+      });
+    }
+    return data;
+  }
+
+  render(): JSX.Element {
+    return (
+      <LanguageContext.Provider
+        value={{
+          'tState': this.state,
+          'tChange': (language: string) => this.changeLanguage(language),
+          't': this.translate,
+        }}>
+        {this.props.children}
+      </LanguageContext.Provider>
+    );
+  }
+}
+
+export const useTranslate = () => React.useContext(LanguageContext);
