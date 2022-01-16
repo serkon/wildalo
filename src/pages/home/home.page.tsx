25880976e62bd4d77978d22b1a785efa85752c3e
Serkan KONAKCI <serkankonakci@gmail.com>
2022-01-16 16:31:47 +0300

    feat: add bear and roadmap sections to landing page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,28 @@
-import { Heading, Container, Text } from '@chakra-ui/react';
+import { Heading, Container, Text, Grid, GridItem } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 import { Hero } from './hero/hero.component';
 import { useTranslate } from 'src/components/translate/translate.component';
 import './home.page.scss';
+
+const roadmap = [
+  {
+    'title': 'Q1',
+    'description': 'Development & Community building, TGE',
+  },
+  {
+    'title': 'Q2',
+    'description': 'Herd Fights begin',
+  },
+  {
+    'title': 'Q3',
+    'description': 'Fall Wildlings Editions (+X new wildlings), League/Tournament',
+  },
+  {
+    'title': 'Q4',
+    'description': 'Winter Wildlings Editions (+Y new wildlings), Wildalo Homesteads',
+  },
+];
 
 export const HomePage = () => {
   const { t } = useTranslate();
@@ -15,7 +34,7 @@ export const HomePage = () => {
       </Heading>
       <Hero />
       <section className="one">
-        <Container maxW="container.md" className="hero" flexDirection="column" textAlign="center">
+        <Container maxW="container.md" flexDirection="column" textAlign="center">
           <Heading as="h1" color="white">
             {t('main.section.one.header')}
           </Heading>
@@ -25,7 +44,33 @@ export const HomePage = () => {
           </Link>
         </Container>
       </section>
-      <section className="second"></section>
+      <section className="second">
+        <Container maxW="container.lg" display="flex" flexDirection="column" textAlign="right" alignItems="flex-end">
+          <Heading as="h3" size="xl" variant="center" isTruncated className="title">
+            Wild Animal World
+          </Heading>
+          <div className="description">
+            Wildalo is a wild world filled with amazing animals also known as wildlings . Wildalo players, aka Rangers, collect these wildlings to grow them (upgrade their levels),
+            form herds with them and fight their herds with other herds to earn in game currency.
+          </div>
+          <div className="stone"></div>
+        </Container>
+      </section>
+      <section className="third">
+        <Container maxW="container.lg" display="flex" flexDirection="column" textAlign="left" alignItems="flex-end">
+          <Grid templateColumns="repeat(4, 1fr)" gap={10} width="100%">
+            {roadmap.map((item, key) => (
+              <GridItem key={key} className="prop">
+                <Heading as="h3" size="xl" h="20" isTruncated className="title">
+                  {item.title}
+                </Heading>
+                <p>{item.description}</p>
+              </GridItem>
+            ))}
+          </Grid>
+          <div className="leaf"></div>
+        </Container>
+      </section>
     </>
   );
 };
