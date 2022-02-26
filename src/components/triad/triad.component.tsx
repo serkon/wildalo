08038f3fed2a2d66c60f687b5dd512de0285bb25
Serkan KONAKCI <serkankonakci@gmail.com>
2022-02-26 20:52:02 +0300

    refactor: add empty view to triad component

--- src/components/triad/triad.component.tsx
+++ src/components/triad/triad.component.tsx
@@ -11,11 +11,15 @@ export const Triad = ({ data }: { data: Animal[] | undefined }) => {
   return (
     <>
       <Container maxW="container.xl" className={`triad ${isDesktop ? 'desktop' : 'mobile'}`}>
-        {data && data.length > 0 && (
+        {data && data.length > 0 ? (
           <Box className={`triad-left-side`}>
             <AnimalCard data={data[0]} scale={0.8} className="animal-first ac" />
             <AnimalCard data={data[1]} scale={1} className="ac animal-middle" />
             <AnimalCard data={data[2]} scale={0.8} className="animal-last ac" />
+          </Box>
+        ) : (
+          <Box opacity={0.6} width="32" textAlign={'center'} lineHeight={'25px'} margin={14} marginBottom="20" fontSize="18px">
+            You don't have a wildling
           </Box>
         )}
       </Container>
