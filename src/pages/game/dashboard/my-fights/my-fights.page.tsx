b1e05ce30fec6a1c9d7b7446dac76f9e581970be
Serkan KONAKCI <serkankonakci@gmail.com>
2022-10-16 20:57:40 +0300

    refactor: update fight overview page to store connection

--- src/pages/game/dashboard/my-fights/my-fights.page.tsx
+++ src/pages/game/dashboard/my-fights/my-fights.page.tsx
@@ -9,7 +9,7 @@ import { useTranslate } from 'src/components/translate/translate.component';
 import { RootState } from 'src/store/store';
 import { useDirection } from 'src/hooks';
 import { LinkGame } from 'src/utils/links';
-import { getFights } from 'src/pages/game/wah/wah.page';
+import { getFightsApi } from 'src/pages/game/wah/wah.page';
 
 export const MyFights = () => {
   const { t } = useTranslate();
@@ -19,7 +19,7 @@ export const MyFights = () => {
 
   useEffect(() => {
     async function fetchData() {
-      await getFights();
+      await getFightsApi();
     }
 
     if (store.metamask.status) {
@@ -28,8 +28,8 @@ export const MyFights = () => {
   }, [store.metamask.status]);
 
   useEffect(() => {
-    ref.current = Math.floor((store.fightsOverview.winScore * 100) / store.fightsOverview.totalScore ?? 1);
-  }, [store.fightsOverview]);
+    ref.current = Math.floor((store.overview.winScore * 100) / store.overview.totalScore ?? 1);
+  }, [store.overview]);
 
   return (
     <div className="my-herds">
@@ -43,32 +43,32 @@ export const MyFights = () => {
           </Text>
           <Flex alignItems={'center'} justifyContent={'center'} width="44px" height="44px" borderRadius="50%" border="1px solid #2A5950" backgroundColor="#0B2F28">
             <Text fontWeight={'bold'} fontSize="24px" lineHeight="28px" marginLeft="-1px" letterSpacing="-1px" color="#77D163">
-              {store.fightsOverview.fights.length ?? 0}
+              {store.overview.fights.length ?? 0}
             </Text>
           </Flex>
         </HStack>
       </Flex>
-      {store.fightsOverview.fights.length > 0 ? (
-        store.fightsOverview.fights.map((_item: FightInterface, key: React.Key | null | undefined) => <Fight key={key} detail={_item} />)
+      {store.overview.fights.length > 0 ? (
+        store.overview.fights.map((_item: FightInterface, key: React.Key | null | undefined) => <Fight key={key} detail={_item} />)
       ) : (
         <Box opacity={0.6} width="56" textAlign={'center'} lineHeight={'25px'} mx="auto" marginTop="14" marginBottom="8" fontSize="18px">
           {t('dashboard.no_fights_found')}
         </Box>
       )}
       {/* TODO: Store'da adamın hiç herds'i yoksa disable et kontrolü ekle */}
-      <Button variant={'primary'} mt="34px" disabled={!store.metamask.status || store.fightsOverview.fights.length >= 3} onClick={() => direction(LinkGame[1])}>
+      <Button variant={'primary'} mt="34px" disabled={!store.metamask.status || store.overview.fights.length >= 3} onClick={() => direction(LinkGame[1])}>
         {t('dashboard.start_new')}!
         <HStack position={'absolute'} right={'16px'} fontSize="12px" fontWeight={'bold'}>
           <Box>{t('dashboard.Remaining')}</Box>
           <Badge color="white" bg="black" borderRadius="50%" width="20px" height="20px" alignItems="center" justifyContent="center" display="flex">
-            {3 - (store.fightsOverview.fights.length ?? 0)}
+            {3 - (store.overview.fights.length ?? 0)}
           </Badge>
         </HStack>
       </Button>
       {ref.current >= 0 && <ChartPie data={ref.current} description="Fight Win Rate" width="207px" height="207px" style={{ marginTop: '67px' }} />}
       <Stack alignItems={'center'}>
         <Box fontWeight={500} fontSize={'19px'} lineHeight="27px">
-          {store.fightsOverview.winScore ?? 0} / {store.fightsOverview.totalScore ?? 1}
+          {store.overview.winScore ?? 0} / {store.overview.totalScore ?? 1}
         </Box>
         <Box fontSize={'10px'} marginTop="-2px!important" lineHeight="14px" opacity={0.3}>
           {t('dashboard.Total_Fights')}
